b2aebeab14a5515f04364540e3ce1174141ad639
James Niyongira <lascasasjames85@gmail.com>
2023-05-11 23:53:26 -0400

    Practicing Multithreading in Java

--- WebApplication2/Pages/Index.cshtml.cs
+++ WebApplication2/Pages/Index.cshtml.cs
@@ -18,36 +18,54 @@ public class IndexModel : PageModel
     public int? Number2 { get; set; }
     [BindProperty]
     public int? Number3 { get; set; }
+    
 
-    public int? Result { get; private set; }
+
+    public int? Total { get; private set; }
     public int? MaxValue { get; private set; }
     public int? MinValue { get; private set; }
+    public double Average { get; private set; }
     
     // declare a list of integers to store the numbers
     public List<int> Numbers { get; set; } = new List<int>();
+    public int Counter = 0;
 
     public void OnPost()
     {
       Console.WriteLine(Number1+Number2+Number3);
-      if (Number1.HasValue)
+      if (Number1.HasValue )
       {
           Numbers.Add(Number1.Value);
+          Counter++;
           
       }
       if (Number2.HasValue)
       {
           Numbers.Add(Number2.Value);
+            Counter++;
             
       }
       if (Number3.HasValue)
       {
           Numbers.Add(Number3.Value);
+            Counter++;
             
       }
 
-      Result = Numbers.Sum();
-      MaxValue = Numbers.Max();
-      MinValue = Numbers.Min();
-      Console.WriteLine(Result);
+      Total = Numbers.Sum();
+      if (Counter == 0)
+      {
+        Total = null;
+      }
+
+      if (Numbers.Count > 0)
+      {
+          MaxValue = Numbers.Max();
+          MinValue = Numbers.Min();
+          // calculate the average to 2 decimal places
+          Average = Math.Round(Numbers.Average(), 2);
+      }
+      
+      Console.WriteLine(Total);
     }
 }
